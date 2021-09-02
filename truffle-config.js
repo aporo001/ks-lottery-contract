b3216cca3f084a67a610bb9b50c22cdadf1e86ee
Sukit Chittawanij <sukit.chi@fullteam.tech>
2021-09-02 22:46:31 +0700

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    matic: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://matic-mumbai.chainstacklabs.com`
+        ),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+  },
+  //
+  compilers: {
+    solc: {
+      version: "0.8.7",
+    },
+  },
+};
